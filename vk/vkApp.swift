aeab399b7f02587712fc73925b793d0b45a2f384
Кожемин Егор <egor.kozhemin@gmail.com>
2022-01-21 00:57:21 +0300

    Initial Commit

---
+++ vk/vkApp.swift
@@ -0,0 +1,17 @@
+//
+//  vkApp.swift
+//  vk
+//
+//  Created by Егор Кожемин on 21.01.2022.
+//
+
+import SwiftUI
+
+@main
+struct vkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
